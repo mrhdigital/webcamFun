2eafcac4b6d9cf97ae512e51884772fa8c507433
unknown <mrhdigital@yahoo.com>
2019-12-22 13:30:28 -0600

    js linked

---
+++ assets/javascript/webcamFun.js
@@ -0,0 +1,6 @@
+alert("js is linked"); 
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
