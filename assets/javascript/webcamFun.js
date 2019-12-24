bb030abf62c9c499e3eaa862ee301ae79f530cc0
unknown <mrhdigital@yahoo.com>
2019-12-23 21:10:50 -0600

    console.log(localMediaStream)

--- assets/javascript/webcamFun.js
+++ assets/javascript/webcamFun.js
@@ -4,3 +4,12 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(localMediaStream => {
+        console.log(localMediaStream);
+    });
+}
+
+getVideo();
