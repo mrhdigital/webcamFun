a26867b6d8957c27b6f2e19c1402d75d39d0f56b
unknown <mrhdigital@yahoo.com>
2019-12-25 14:03:44 -0600

    created ghosting effect with ctx.globalApha

--- assets/javascript/webcamFun.js
+++ assets/javascript/webcamFun.js
@@ -33,6 +33,7 @@ function paintToCanvas() {
        // mess with them
        //pixels = redEffect(pixels);
        pixels = rgbSplit(pixels);
+       ctx.globalAlpha = 0.1;
        // put them back
        ctx.putImageData(pixels, 0, 0);
 
@@ -68,8 +69,8 @@ function redEffect(pixels) {
 function rgbSplit(pixels) {
     for (let i = 0; i < pixels.data.length; i+=4) {
       pixels.data[i - 150] = pixels.data[i + 0]; // RED
-      pixels.data[i + 100] = pixels.data[i + 1]; // GREEN
-      pixels.data[i - 150] = pixels.data[i + 2]; // Blue
+      pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+      pixels.data[i - 550] = pixels.data[i + 2]; // Blue
     }
     return pixels;
 }
